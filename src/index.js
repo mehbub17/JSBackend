588a7077133d5c307429366fb8aa147613c37f68
mehbub17 <mehbubhussain.17@gmail.com>
2025-04-25 23:41:07 +0530

    Utility code

--- src/index.js
+++ src/index.js
@@ -15,6 +15,14 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log('mongodb connection failed',errr)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+
+}
+
+
+
+
+
+
+export {asyncHandler}
+
+
+
+//using try-catch wrapper
+
+// const asyncHandler = (fn) => async(req,res,next) =>{
+
+//     try {
+//         await fn(req,res,next);
+        
+//     } catch (error) {
+
+//             res.status(error.code || 500).json({
+//                 success: false,
+//                 message: error.message
+//             })
+        
+//     }
+
+
+// }
